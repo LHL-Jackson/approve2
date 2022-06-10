b4cce74f22a7bfc655637c15920ef2b1229e1016
Jackson Lee <Jackson@lhlconsulting.com.sg>
2022-06-10 09:27:08 +0800

    v10-Jun

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <div v-cloak>
+  
   <hr/>
-  <label style="font-size:20px"> {{splitData(0)}} - Approval App. </label>
+  <label> {{urlData(0)}} - Approval App. ({{appLevel}}) </label>
   <hr/>
-  <label style="font-size:20px"> Customer Name:  &#160  {{splitData(2)}}  </label>
+  <label> Customer Name:  &#160  {{urlData(3)}}  </label>
   <hr/>
-  <label  style="font-size:20px"> Order Number:  &#160  </label> 
-    <a  style="font-size:25px" href="https://ap.lhlconsulting.com/a.pdf"  target="_blank"> {{splitData(1)}}   </a>
+  <label> Order Number:  &#160  </label> 
+    <a  href="https://ap.lhlconsulting.com/a.pdf"  target="_blank"> {{urlData(1)}}   </a>
   <hr/>
-  <p style="font-size:20px">  Please view from the above link </p> 
-  <p style="font-size:20px">  and select yout approval carefully. </p> 
+  
+  <p>  {{appMessage()}} </p> 
   <hr/>
-  <p><label  style="font-size:20px"> Remark   &#160  </label> </p>
-  <input style="font-size:20px; width: 500px; "  type="text" v-model="remark"  >
+  <p><label> Remark   &#160  </label> </p>
+  <input  type="text" v-model="remark"  >
   <hr/>
-  <button style="height:50px;width:120px; font-size: 20px; margin-right:20px;" @click.prevent="approved">Approve</button>
-  <button style="height:50px;width:120px; font-size: 20px; margin:10px;" @click.prevent="rejected">Reject</button>
-   <hr/>
-  <input style="font-size:15px; width: 500px; "  type="text" v-model="urlApp"  >
+  <button @click.prevent="approved">Approve</button>
+  <button style="margin-left:25px;" @click.prevent="rejected">Reject</button>
+   
+  </div>
   </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-//https://localhost:8081/index.html?so=D1001&cmp=ABM P/L
+// params: 1=ZP, 2=OrdNo, 3=ApproveLevel, 4=CustName, 
+//https:ap.lhlconsulting.com/app/?data=TEhMQ08xPT1PUkQxPT0xPT1hYmMgVHJhZGluZyBQL0w= (level-1)
+//https:ap.lhlconsulting.com/app/?data=TEhMQ08xPT1PUkQxPT0yPT1hYmMgVHJhZGluZyBQL0w=   (level-2)
+//http://localhost:8080/app/?data=data=TEhMQ08xPT1PUkQxPT0xPT1hYmMgVHJhZGluZyBQL0w=
+
 export default {
   name: 'App',
   props: {
@@ -34,19 +39,33 @@ export default {
       soNo:  '',    // new URL(location.href).searchParams.get('so'),
       cmpName: '',  //new URL(location.href).searchParams.get('cmp'),
       remark: '',  //new URL(location.href).searchParams.get('cmp'),
-      urlApp: ""
+      appLevel: '1',
     }
   },
   //components: { HelloWorld }
   methods: {
-    splitData(index) {
+    urlData(index) {
        const encoded64Data = new URL(location.href).searchParams.get('data')
        const plainTextData = window.atob(encoded64Data); // decode a string from base64
        let data1 = plainTextData.split("==");
        return data1[index]
      },
+
+    appMessage() {
+      var msg ='Please check Draft Invoice carefully.' 
+      const encoded64Data = new URL(location.href).searchParams.get('data')
+      const plainTextData = window.atob(encoded64Data); // decode a string from base64
+      let data1 = plainTextData.split("==");
+      if (data1[2]!="1") {
+        msg ='Please Approve/Reject Draft Invoice carefully.'
+      } 
+      this.appLevel=data1[2]
+      return msg  
+    },
+     // params: 1=ZP, 2=OrdNo, 3=Approved/Rejected, 4=ApproveLevel, 5=Remark 
     approved() {
-            const plainText = this.splitData(0) + "==" + this.splitData(1)  + "==Approved" + "==" + this.remark
+            const plainText = this.urlData(0) + "==" + this.urlData(1) 
+                             + "==Approved" + "==" + this.appLevel +"==" + this.remark
       const encoded64 = window.btoa(plainText); // encode a string
       const url = 'https://ap.lhlconsulting.com/approval/api/approve?data=' + encoded64
       this.urlApp =url
@@ -66,7 +85,7 @@ export default {
     },
 
     rejected() {
-      const plainText = this.splitData(0) + "==" + this.splitData(1) + "==Rejected"  + "==" + this.remark
+      const plainText = this.urlData(0) + "==" + this.urlData(1) + "==Rejected"  + "==" + this.appLevel +"==" + this.remark
       const encoded64 = window.btoa(plainText); // encode a string
       const url = 'https://ap.lhlconsulting.com/approval/api/approve?data=' + encoded64
       //https://ap.lhlconsulting.com/approval/api/approve?data=UzAyMDAxPT1BQkMgUC9M
@@ -85,9 +104,7 @@ export default {
       window.close()  
     }
     
-    
   }
-
 
 }
 </script>
@@ -101,5 +118,27 @@ export default {
   color: #2c3e50;
   margin-top: 20px;
   margin-left: 20px;
+}
+body {
+  font-size: 20px;
+}
+
+input[type="text"] {
+  height: 30px;
+  width: 400px;
+  margin-top: 0px 0;
+  display: block; 
+}
+
+button {
+  font-size: 20px;
+  height: 50px;
+  width: 110px;
+  /* padding: 10px; */
+}
+
+
+[v-cloak] {
+  display: none;
 }
 </style>
